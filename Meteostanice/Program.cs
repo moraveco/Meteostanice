5621ef91e8501232a3a67bd895d57d9babbac6dd
Ondřej Moravec <92747275+moraveco@users.noreply.github.com>
2026-06-06 18:06:06 +0200

    updated Program.cs - DI, EF, Serilog, auto migration

--- Meteostanice/Program.cs
+++ Meteostanice/Program.cs
@@ -1,7 +1,41 @@
 using Meteostanice;
+using Meteostanice.Api;
+using Meteostanice.Data;
+using Meteostanice.Services;
+using Meteostanice.Settings;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.EntityFrameworkCore;
+using Serilog;
 
-var builder = Host.CreateApplicationBuilder(args);
+var builder = WebApplication.CreateBuilder(args);
+
+// Serilog
+builder.Host.UseSerilog((context, config) =>
+    config.ReadFrom.Configuration(context.Configuration));
+
+// Settings
+builder.Services.Configure<MeteoSettings>(
+    builder.Configuration.GetSection("MeteoSettings"));
+
+// Database
+builder.Services.AddDbContext<MeteoDbContext>(options =>
+    options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+// Services
+builder.Services.AddHttpClient<MeteoFetcher>();
+builder.Services.AddSingleton<MeteoParser>();
 builder.Services.AddHostedService<Worker>();
 
-var host = builder.Build();
-host.Run();
+var app = builder.Build();
+
+// Automatic migration
+using (var scope = app.Services.CreateScope())
+{
+    var db = scope.ServiceProvider.GetRequiredService<MeteoDbContext>();
+    db.Database.Migrate();
+}
+
+// Endpointy
+app.MapDashboardEndpoints();
+
+app.Run();
